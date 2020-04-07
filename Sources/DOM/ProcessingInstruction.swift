029ad8c1115ab32b7c20ab52eb092fbc030deb17
Mattt <mattt@me.com>
2020-04-07 06:31:56 -0700

    Add target property to ProcessingInstruction

--- Sources/DOM/ProcessingInstruction.swift
+++ Sources/DOM/ProcessingInstruction.swift
@@ -1,9 +1,21 @@
 import libxml2
 
 public final class ProcessingInstruction: Node {
+    public var target: String {
+        get {
+            return String(cString: xmlNode.pointee.name)
+        }
+
+        set {
+            xmlNodeSetName(xmlNode, newValue)
+        }
+    }
+
     public func remove() {
         unlink()
     }
+
+    // MARK: -
 
     public convenience init?(name: String, content: String?) {
         guard let xmlNode = xmlNewPI(name, content) else { return nil }
